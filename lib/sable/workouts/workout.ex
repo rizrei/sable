427f49f1cbc2dfcb1452c20721a3efa1ce86f9fd
Albert Gabitov <agabitov@evrone.com>
2025-11-24 18:21:11 +0300

    Enables drag-and-drop reordering of exercises
    Adds SortableJS library for drag-and-drop functionality.
    Allows users to reorder exercises within a workout.
    Persists the exercise order in the database.
    
    Adds a new "reposition" event to handle the reordering.

--- lib/sable/workouts/workout.ex
+++ lib/sable/workouts/workout.ex
@@ -18,7 +18,7 @@ defmodule Sable.Workouts.Workout do
 
     belongs_to :author, Sable.User
 
-    has_many :workout_exercises, Sable.Workouts.WorkoutExercise
+    has_many :workout_exercises, Sable.Workouts.WorkoutExercise, on_replace: :delete
     has_many :workout_tags, Sable.Workouts.WorkoutTag, on_replace: :delete
     has_many :tags, through: [:workout_tags, :tag]
     has_many :exercises, through: [:workout_exercises, :exercise]
@@ -36,6 +36,12 @@ defmodule Sable.Workouts.Workout do
       drop_param: :workout_tags_drop,
       required: false
     )
+    |> cast_assoc(:workout_exercises,
+      with: &workout_exercise_changeset/2,
+      sort_param: :workout_exercises_sort,
+      drop_param: :workout_exercises_drop,
+      required: false
+    )
     |> validate_required([:title, :description, :author_id])
     |> foreign_key_constraint(:author_id)
   end
@@ -44,5 +50,11 @@ defmodule Sable.Workouts.Workout do
     workout_tag
     |> cast(attrs, [:tag_id])
     |> validate_required([:tag_id])
+  end
+
+  defp workout_exercise_changeset(workout_exercise, attrs) do
+    workout_exercise
+    |> cast(attrs, [:exercise_id, :position])
+    |> validate_required([:exercise_id])
   end
 end
